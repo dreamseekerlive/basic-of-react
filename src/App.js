f013b2bca301d69ec9830d38bee474359a12121a
dreamseeker <dreamseeker.live@gmail.com>
2023-06-02 10:31:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	  return (
+		      <div className="App">
+		        <h1>Welcome to BasicOfReact!</h1>
+		        <p>This is a basic React app.</p>
+		      </div>
+		    );
+}
+
+export default App;
+
